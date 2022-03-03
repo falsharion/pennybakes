1e6e31d35dd2ccc65361fc718f869e014cd38399
falsharion <92723282+falsharion@users.noreply.github.com>
2022-03-03 19:57:35 +0100

    files
    adding files

---
+++ myScript.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+    burger.addEventListener('click', () => {
+        //toggle nav
+        nav.classList.toggle('nav-active');
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //burger animaation
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+
+navSlide();
+
